ddc3635b8c809412e9eeb324050dac8a359cace5
Anant Bhardwaj <anantb@csail.mit.edu>
2014-08-14 11:07:26 -0700

    add types

--- src/django_server/server/static/js/search_loader.js
+++ src/django_server/server/static/js/search_loader.js
@@ -4,11 +4,24 @@
 */
 
 var service = new google.maps.places.AutocompleteService();
+
+function get_type_code(types){
+  if (types.indexOf("country") > -1) {
+    return 0;
+  } else if (types.indexOf("administrative_area_level_1") > -1) {
+    return 1;
+  } else if (types.indexOf("administrative_area_level_2") > -1) {
+    return 2;
+  }
+  return -1;
+}
+
 function load_search(input_id) {
   $(input_id).autocomplete({
     source: function(request, response) {
       var search_params = {input: request.term, types: ['(regions)']}           
       service.getPlacePredictions(search_params, function(data) {
+        console.log(data)
         if (data == null) {
           return response([])
         }          
@@ -21,7 +34,7 @@ function load_search(input_id) {
         }));
     })},
     select: function(e, ui) {
-      window.location.href = '/p?place=' + encodeURIComponent(ui.item.value);
+      window.location.href = '/p?place=' + encodeURIComponent(ui.item.value) + '&type=' + encodeURIComponent(get_type_code(ui.item.data.types));
     }
   }).data("ui-autocomplete")._renderItem = function (ul, item) {
     return $("<li></li>")
@@ -32,9 +45,9 @@ function load_search(input_id) {
 }
 
 function format_result(item) {
-  var result = '<span class="maps-suggest-title">' +  item.data.terms[0].value + '</span>';
+  var result = '<span class="autocomplete-suggest-title">' +  item.data.terms[0].value + '</span>';
   for (var i = 1; i < item.data.terms.length; i++) {
-    result += '<span class="maps-suggest-details">' +  item.data.terms[i].value + '</span>';
+    result += '<span class="autocomplete-suggest-details">' +  item.data.terms[i].value + '</span>';
   }
   return result
 }
--- src/django_server/server/views.py
+++ src/django_server/server/views.py
@@ -14,5 +14,20 @@ def search(request):
   return render_to_response("search.html")
 
 def place(request):
-  p = {'place': request.REQUEST['place']}
+  p = {}
+  if 'place' not in request.REQUEST:
+    return HttpResponse("Missing place value in the query")
+  
+  place = request.REQUEST['place']
+  if place.strip() == '':
+    return HttpResponse("Empty value for place")
+  
+  p['place'] = place
+
+  if 'type' in request.REQUEST:
+    try:
+      p['type'] = int(request.REQUEST['type'])
+    except:
+      p['type'] = -1
+  
   return render_to_response("place.html", p)
